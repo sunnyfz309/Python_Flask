b1b60bb0b479c157f5ed93f2c04af917cf931216
sunnyfz309 <sunnyfz309@gmail.com>
2019-03-22 15:38:01 -0700

    email and pwd

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -8,21 +8,18 @@ from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
-    	validators=[DataRequired(), Length(min=2, max=20)])
+                           validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
-    	validators=[DataRequired(), Email()])
-    password = PasswordField('Password', 
-    	validators=[DataRequired()])
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
-    	validators=[DataRequired(), EqualTo('password')])
+                                     validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
-
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('That username is taken. Please choose a different one.')
-
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
@@ -32,28 +29,25 @@ class RegistrationForm(FlaskForm):
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
-    	validators=[DataRequired(), Email()])
-    password = PasswordField('Password', 
-    	validators=[DataRequired()])
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
 
 class UpdateAccountForm(FlaskForm):
     username = StringField('Username',
-        validators=[DataRequired(), Length(min=2, max=20)])
+                           validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
-        validators=[DataRequired(), Email()])
+                        validators=[DataRequired(), Email()])
     picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
-
 
     def validate_username(self, username):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
                 raise ValidationError('That username is taken. Please choose a different one.')
-
 
     def validate_email(self, email):
         if email.data != current_user.email:
@@ -68,26 +62,19 @@ class PostForm(FlaskForm):
     submit = SubmitField('Post')
 
 
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first.')
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
